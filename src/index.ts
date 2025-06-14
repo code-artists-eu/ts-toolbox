b0766f6047822261e5129920c13f894ed7aefd5c
Matej Puhony <matej.puhony@seznam.cz>
2025-06-14 16:59:29 +0200

    Add export of calendar fnc

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import * as latLon from "./map/lat-long"
 import * as mapTiles from "./map/map-tiles"
 import * as array from "./array/array"
 import * as files from "./files/files"
+import * as calendar from "./dates/calendar"
 
 export default {
   math,
   latLon,
   mapTiles,
   array,
-  files
+  files,
+  calendar
 }
